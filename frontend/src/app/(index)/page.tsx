5fb6e69eaa8bcf3f2e4f22ce08413b31a95b322e
Roel Marasigan <roelm07@yahoo.com>
2024-08-12 16:04:28 -0600

    updated Navigation Page

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -1,5 +1,15 @@
+import {Navigation} from "@/app/components/Navigation";
+
 export default function Home() {
     return (
+
+        <>
+            <Navigation />
         <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
+
+
+
+
+        </>
     )
 }
