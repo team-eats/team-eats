28960485a14910b1f3990b63060102832259eb93
Rusty Libby <rlibby@cnm.edu>
2024-08-18 15:48:04 -0600

    fix nav

--- frontend/src/app/about-us/page.tsx
+++ frontend/src/app/about-us/page.tsx
@@ -1,13 +1,10 @@
-export default function(){
-<<<<<<< HEAD
-    return(
-        <h1>hey man whats up</h1>
+export default function() {
+    return (
+
+        <>
+            <h1>hey man whats up</h1>
+        </>
+
     )
-=======
-     return (
-               <h1>hey man whats up</h1>
-     )
 
-
->>>>>>> develop
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,9 +16,11 @@ export default function RootLayout(props : RootLayoutProps) {
     const { children } = props
     return (
         <html  lang="en" suppressHydrationWarning>
-        <Navigation />
 
-        <body>{children}</body>
+        <body>
+        <Navigation />
+        {children}
+        </body>
         </html>
     )
 }
