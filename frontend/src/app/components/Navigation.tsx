6c3356c88ab326c03ca18af066129c40007f1954
Rusty Libby <rlibby@cnm.edu>
2024-08-25 01:38:11 -0600

    got the favorite modal done

--- frontend/src/app/components/Navigation.tsx
+++ frontend/src/app/components/Navigation.tsx
@@ -2,6 +2,7 @@
 
 import {Button, Modal, Dropdown, Navbar} from "flowbite-react";
 import { useState } from "react";
+import {BusinessCard} from "@/app/components/BusinessCard";
 
 
 
@@ -73,25 +74,20 @@ export function Navigation() {
 
 
     <Modal dismissible show={openModal} onClose={() => setOpenModal(false )}>
-        <Modal.Header>Terms of Service</Modal.Header>
-        <Modal.Body>
-            <div className="space-y-6">
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-                    companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
-                    to ensure a common set of data rights in the European Union. It requires organizations to notify users as
-                    soon as possible of high-risk data breaches that could personally affect them.
-                </p>
+        <Modal.Header className={"bg-orange-200"}>Favorite Eats</Modal.Header>
+        <Modal.Body className={"bg-red-700"}>
+
+            <div>
+                <BusinessCard />
+                <BusinessCard />
+                <BusinessCard />
+                <BusinessCard />
+                <BusinessCard />
+                <BusinessCard />
             </div>
         </Modal.Body>
-        <Modal.Footer>
-            <Button onClick={() => setOpenModal(false)}>I accept</Button>
-            <Button color="gray" onClick={() => setOpenModal(false)}>
-                Decline
-            </Button>
+        <Modal.Footer className={"bg-orange-200"}>
+
         </Modal.Footer>
     </Modal>
 </>
