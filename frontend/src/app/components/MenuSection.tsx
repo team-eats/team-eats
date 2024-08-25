d217cf1ab01ad8f74d03b4664f6b0c09dc3e0ffa
Evan Bolton <ebolton2@cnm.edu>
2024-08-25 10:50:46 -0600

    Finished the business listing page

--- frontend/src/app/components/MenuSection.tsx
+++ frontend/src/app/components/MenuSection.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-import {MenuItems} from "@/app/components/MenuItems";
+import {MenuItem} from "@/app/components/MenuItem";
 
 export function MenuSection() {
     return(
         <>
-            <h3>Appetizers</h3>
-            <MenuItems />
+            <h3 className='text-2xl'>Appetizers</h3>
+            <div className='overflow-x-auto flex gap-3'>
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+            </div>
         </>
     )
 }
