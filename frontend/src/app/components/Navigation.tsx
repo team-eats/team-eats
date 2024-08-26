78c030460b1d8d1e116e1c996cbb4bbd1879e35e
Evan Bolton <ebolton2@cnm.edu>
2024-08-26 08:08:58 -0600

    Nav bar fiddling

--- frontend/src/app/components/Navigation.tsx
+++ frontend/src/app/components/Navigation.tsx
@@ -28,7 +28,7 @@ export function Navigation () {
                                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
                                            strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                 </svg>
+                                 </svg>g
                                  <span className="sr-only">Search icon</span>
                              </div>
                              <input type="text" id="search-navbar"
