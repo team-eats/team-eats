e5fb3e009c553e36405df23bbc467b7746b00f88
Rusty Libby <rlibby@cnm.edu>
2024-08-26 11:30:04 -0600

    undoing

--- frontend/src/app/components/Navigation.tsx
+++ frontend/src/app/components/Navigation.tsx
@@ -7,7 +7,6 @@ import {BusinessCard} from "@/app/components/BusinessCard";
 
 
 
-
 export function Navigation() {
     const [openModal, setOpenModal] = useState(false);
 
@@ -36,7 +35,16 @@ export function Navigation() {
                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:border-red-800 focus:ring-red-800 focus:border-red-800 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 px-14"
                                placeholder="Search Eats..." required/>
                     </div>
-
+                    <button type="submit"
+                            className="p-2.5 ms-2 text-sm font-medium text-white bg-black rounded-lg border border-black hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-white dark:bg-red-600 dark:hover:bg-red-800 dark:focus:ring-red-500">
+                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                  stroke-width="2"
+                                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                        <span className="sr-only">Search</span>
+                    </button>
                 </form>
 
 
