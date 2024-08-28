24efdf5b949a1bb4c2560fdde00f5f3f0b4f3db9
Cooper Mcgaffey <lmcgaffey@cnm.edu>
2024-08-27 22:40:27 -0600

    styling form elements

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -8,11 +8,12 @@ import Link from "next/link";
 export default function() {
     return (
         <>
-            <div className="flex flex-col md:flex-row justify-around p-6 my-auto md:py-20 gap-16 md:gap">
+            <div
+                className="flex flex-col justify-evenly items-center md:flex-row my-12 md:my-auto md:py-20 md:justify-around gap-12 md:gap-0">
 
 
                 <div>
-                    <form className="flex max-w-md flex-col gap-4 border-2 border-gray-600 p-3 rounded-lg">
+                    <form className="flex w-72 md:w-[20rem] flex-col gap-4 border-2 border-gray-600 p-5 rounded-lg">
                         <h2 className="text-2xl">Login</h2>
                         <div>
                             <div className="mb-2 block">
@@ -28,18 +29,23 @@ export default function() {
                             <TextInput id="password2" type="password" required shadow/>
                         </div>
                         <Button type="submit"
-                        className="my-3l">Login</Button>
+                                className="my-3l">Login</Button>
                     </form>
                 </div>
 
                 <div>
-                    <form className="flex max-w-md flex-col gap-4 border-2 border-gray-600 p-3 rounded-lg">
+                    <p>or</p>
+                </div>
+
+                <div>
+                    <form
+                        className="flex text-2xl w-[20rem] md:w-[20rem] flex-col gap-4 border-2 border-gray-600 p-5 rounded-lg">
                         <h2>Create Account</h2>
                         <div>
                             <div className="mb-2 block">
                                 <Label htmlFor="email2" value="Your email"/>
                             </div>
-                            <TextInput id="email2" type="email" placeholder="name@flowbite.com" required shadow/>
+                            <TextInput id="email2" type="email" placeholder="email@address.com" required shadow/>
                         </div>
                         <div>
                             <div className="mb-2 block">
@@ -53,14 +59,16 @@ export default function() {
                             </div>
                             <TextInput id="repeat-password" type="password" required shadow/>
                         </div>
-                        <div className="flex items-center gap-2">
+                        <div className="flex items-center">
                             <Checkbox id="agree"/>
-                            <Label htmlFor="agree" className="flex">
-                                I agree with the&nbsp;
-                                <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                                    terms and conditions
-                                </Link>
-                            </Label>
+                            <div className="block px-1">
+                                <Label htmlFor="agree" className="flex">
+                                    I agree with the
+                                    <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500">
+                                        terms and conditions
+                                    </Link>
+                                </Label>
+                            </div>
                         </div>
                         <Button type="submit">Register new account</Button>
                     </form>
