d859a571435b95d5b2623753852bd3c8f1673eeb
Rusty Libby <rlibby@cnm.edu>
2024-09-07 14:25:11 -0600

    finished location

---
+++ backend/src/apis/location/location.route.ts
@@ -0,0 +1,22 @@
+import {Router} from "express";
+import {isLoggedInController} from "../../utils/controllers/isLoggedIn.controller";
+import {
+    deleteLocationByLocationIdController,
+    getLocationByLocationBusinessIdController,
+    postLocationController,
+    putLocationController
+} from "./location.controller";
+
+const basePath = 'apis/location'
+
+const router = Router()
+
+router.route('/')
+.post(isLoggedInController, postLocationController)
+
+router.route('/locationBusinessId/:locationBusinessId')
+.get(getLocationByLocationBusinessIdController)
+
+router.route('/:locationId')
+.put(putLocationController)
+.delete(isLoggedInController, deleteLocationByLocationIdController)
