c6d4777786b6d282b1c906483872d71cf66a511e
Rusty Libby <rlibby@cnm.edu>
2024-09-09 16:40:48 -0600

    started business model file

---
+++ frontend/src/app/utils/models/business.model.ts
@@ -0,0 +1,97 @@
+import {z} from "zod";
+import {unstable_noStore as noStore} from "next/cache";
+
+
+export const BusinessSchema = z.object({
+    businessId: z.string({
+        required_error: 'please provide a valid business id or null',
+        invalid_type_error: 'businessId must be a uuid or null'})
+        .uuid({message: 'please provide a valid uuid for business id'})
+        .nullable(),
+
+    businessProfileId: z.string({
+        required_error: 'please provide a valid businessProfile id',
+        invalid_type_error: 'businessProfileId must be uuid'})
+        .uuid({message: 'please provide a valid uuid for businessProfile id'}),
+
+    businessName: z.string({
+        required_error: 'Please provide a business name',
+        invalid_type_error: 'business name must be a string'})
+        .max(100, {message: 'please provide a shorter business name'})
+        .min(1, {message: 'please make a longer business name'}),
+
+    businessPhoto: z.string({
+        required_error: 'please provide a valid business photo address or null',
+        invalid_type_error: 'business image url must be a string or null'})
+        .trim()
+        .url({message: 'business image address must be url'})
+        .max(255, {message: 'please provide a business photo url shorter than 255'})
+        .nullable(),
+
+    businessHours: z.string({
+        required_error: 'please provide business hours',
+        invalid_type_error: 'business hours must be a string'})
+        .max(255, {message: 'business hours reached character limit'}),
+
+    businessBio: z.string({
+        required_error: 'please provide a business bio',
+        invalid_type_error: 'business bio must be a string'})
+        .max(800, {message: 'business bio must be a lil shorter'}),
+
+    businessEmail: z.string({
+        required_error: 'business must have an email',
+        invalid_type_error: 'business email must be a string'})
+        .email('please provide a valid email address')
+        .max(255, {message: 'please provide a shorter email address'}),
+
+    businessPhone: z.string({
+        required_error: 'please provide a phone number',
+        invalid_type_error: 'business phone must be a string'})
+        .max(10, {message: 'business phone too long'})
+        .min(10, {message: 'business phone too short'})
+})
+
+export type Business = z.infer<typeof BusinessSchema>
+
+
+export async function fetchAllBusinesses() : Promise<Business[]> {
+    noStore()
+    const {data} = await fetch(`${process.env.PUBLIC_API_URL}/apis/business`, {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response: Response) => {
+        if (!response.ok) {
+            throw new Error('Error fetching all businesses')
+        } else {
+            return response.json()
+        }
+
+
+    })
+
+    return BusinessSchema.array().parse(data)
+}
+
+
+export async function fetchBusinessByBusinessId(businessId: string): Promise<Business> {
+    noStore()
+    const {data} = await fetch(`${process.env.PUBLIC_API_URL}/apis/businessId/${businessId}`, {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response: Response) => {
+        if (!response.ok) {
+            throw new Error('Error fetching business by businessId')
+        } else {
+            return response.json()
+        }
+    })
+
+    return BusinessSchema.parse(data)
+}
+
+
+export async function fetchBusinessByBusinessProfileId
