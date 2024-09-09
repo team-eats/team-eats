4a5d9e575d58aae79ffea38bbdb24de8d7220379
Evan Bolton <ebolton2@cnm.edu>
2024-09-09 15:35:07 -0600

    finished frontend profile model

--- frontend/src/app/utils/models/profile.model.ts
+++ frontend/src/app/utils/models/profile.model.ts
@@ -46,5 +46,20 @@ export async function fetchProfileByProfileId(profileId: string): Promise<Profil
     return ProfileSchema.parse(data)
 }
 
+export async function fetchProfileByProfileName(profileName: string): Promise<Profile | null> {
+    const {data} = await fetch(`${process.env.PUBLIC_API_URL}/apis/profile/profileName/${profileName}`, {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response: Response) => {
+        if (!response.ok) {
+            throw new Error('Error fetching profile by profileName')
+        } else {
+            return response.json()
+        }
+    })
 
+    return ProfileSchema.nullable().parse(data)
+}
 
