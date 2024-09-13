c1487e019942c42ea5a86700c7c195e05826ed77
Rusty Libby <rlibby@cnm.edu>
2024-09-13 17:08:19 -0600

    no mo BS dude

--- frontend/src/app/listing-form/listing-form.tsx
+++ frontend/src/app/listing-form/listing-form.tsx
@@ -25,16 +25,16 @@ type BusinessListing = z.infer<typeof businessListingSchema>
 
 type Props = {session: Session}
 
-export function ListingForm(props: Props){
-   const session = props.session;
+export function ListingForm(props: Props) {
+    const session = props.session;
 
 
     const initialValues = {
-        businessName:'',
-        businessHours:'',
-        businessBio:'',
-        businessEmail:'',
-        businessPhone:''
+        businessName: '',
+        businessHours: '',
+        businessBio: '',
+        businessEmail: '',
+        businessPhone: ''
     }
 
     const handleSubmit = (values: BusinessListing, actions: FormikHelpers<BusinessListing>) => {
@@ -52,7 +52,7 @@ export function ListingForm(props: Props){
         const {setStatus, resetForm} = actions
 
         if (values.businessPhoto) {
-            fetch("/apis/image/",{
+            fetch("/apis/image/", {
                 method: "POST",
                 headers: {
                     'Authorization': session.authorization ?? ""
@@ -62,7 +62,7 @@ export function ListingForm(props: Props){
                 .then(response => response.json())
                 .then(json => {
                     if (json.status !== 200) {
-                    setStatus({type: 'failure', message: json.message})
+                        setStatus({type: 'failure', message: json.message})
                     } else {
                         newValues.businessPhoto = json.message
                         postBusiness()
@@ -74,41 +74,39 @@ export function ListingForm(props: Props){
         }
 
 
-function postBusiness() {
-        fetch(`/apis/business`, {
-            method: 'POST',
-            headers: {
-                'Authorization':session.authorization,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(values)
-        })
-            .then(response => response.json())
-            .then(data => {
-                let type = 'failure'
-                if (data.status === 200) {
-                    type = 'success'
-                    resetForm()
-                }
-                setStatus({type, message:data.message})
+        function postBusiness() {
+            fetch(`/apis/business`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': session.authorization,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
             })
-            .catch(error => {
-                console.log(error)
-                setStatus({type: 'failure', message:'input is an error, try again.'})
+                .then(response => response.json())
+                .then(data => {
+                    let type = 'failure'
+                    if (data.status === 200) {
+                        type = 'success'
+                        resetForm()
+                    }
+                    setStatus({type, message: data.message})
+                })
+                .catch(error => {
+                    console.log(error)
+                    setStatus({type: 'failure', message: 'input is an error, try again.'})
 
-            })
-
+                })
+            return (
+                <>
+                    <Formik initialValues={initialValues} onSubmit={handleSubmit}
+                            validationSchema={toFormikValidationSchema(businessListingSchema)}>
+                        {BusinessFormContent}
+                    </Formik>
+                </>
+            )
+        }
     }
-
-    return (
-        <>
-    <Formik
-        initialValues=
-            {initialValues} onSubmit ={handleSubmit} validationSchema={toFormikValidationSchema(businessListingSchema)}>
-        {BusinessFormContent}
-    </Formik>
-    </>
-)
 }
 
 export function BusinessFormContent(props: FormikProps<BusinessListing>) {
@@ -126,109 +124,107 @@ export function BusinessFormContent(props: FormikProps<BusinessListing>) {
         setFieldTouched
     } = props;
 
-    const [selectedImage, setSelectedImage] = React.useState<string | null> (null)
+    const [selectedImage, setSelectedImage] = React.useState<string | null>(null)
 
     return (
         <>
-            <form onSubmit = {handleSubmit} className="">
-                {selectedImage?<img src={selectedImage} alt={"uploadedImage"} />: <></>}
-    <div>
-        <div>
-            <Label htmlFor="businessName" value="Business Name"/>
-        </div>
-        <TextInput
-    onChange={handleChange}
-    onBlur={handleBlur}
-    autoComplete='organization'
-    id="businessName"
-    name={'businessName'}
-    type='text'
-    value={values.businessName}
-    />
-    <DisplayError errors={errors} touched={touched} field={'businessName'}/>
-    </div>
-    <div className="">
-    <Label htmlFor="businessPhoto" value="business photo"/>
-        </div>
-        <ImageUploadDropZone
-    formikProps={{
-        setFieldError,
-            setFieldTouched,
-            handleBlur,
-            handleChange,
-            setFieldValue,
-            fieldValue: 'businessPhoto'}}
-    setSelectedImage={setSelectedImage}
-    />
-
-    <DisplayUploadErrorProps errors={errors} field={'businessPhoto'}/>
-
-
-    <div>
-    <div>
-        <Label htmlFor="businessHours" value="Business Hours" />
-        </div>
-        <TextInput
-    onChange={handleChange}
-    onBlur={handleBlur}
-    autoComplete='organization'
-    id="businessHours"
-    name={"businessHours"}
-    type='text'
-    value={values.businessHours}
-    />
-    </div>
-    <DisplayError errors={errors} touched={touched} field={'businessHours'} />
-    <div>
-    <div>
-        <Label htmlFor="businessBio" value="Business Bio" />
-        </div>
-        <TextInput
-    onChange={handleChange}
-    onBlur={handleBlur}
-    autoComplete='organization'
-    id="businessBio"
-    name={"businessBio"}
-    type='text'
-    value={values.businessBio}
-    />
-    </div>
-    <DisplayError errors={errors} touched={touched} field={'businessBio'} />
-    <div>
-    <div>
-        <Label htmlFor="businessEmail" value="Business Email" />
-        </div>
-        <TextInput
-    onChange={handleChange}
-    onBlur={handleBlur}
-    autoComplete='email'
-    id='businessEmail'
-    name={"businessEmail"}
-    type="email"
-    value={values.businessEmail}
-    />
-    <DisplayError errors={errors} touched={touched} field={'businessEmail'}/>
-    </div>
-    <div>
-    <div className='mb-2 block'>
-    <Label htmlFor="businessPhone" value="Business Phone" />
-        </div>
-        <TextInput
-    onChange={handleChange}
-    onBlur={handleBlur}
-    autoComplete='phone'
-    id="businessPhone"
-    name={'businessPhone'}
-    type="text"
-    value={values.businessPhone}
-    />
-    <DisplayError errors={errors} touched={touched} field={'businessPhone'} />
-    </div>
-        <Button color={'success'} type="submit">Submit</Button>
-        <Button color={'failure'} type="reset" onClick={handleReset}>Reset</Button>
-        <DisplayStatus status={status}/>
-    </form>
-    <FormDebugger {...props} />
-    </>
-)
+            <form onSubmit={handleSubmit} className="">
+                {selectedImage ? <img src={selectedImage} alt={"uploadedImage"}/> : <></>}
+                <div>
+                    <div>
+                        <Label htmlFor="businessName" value="Business Name"/>
+                    </div>
+                    <TextInput
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        autoComplete='organization'
+                        id="businessName"
+                        name={'businessName'}
+                        type='text'
+                        value={values.businessName}
+                    />
+                    <DisplayError errors={errors} touched={touched} field={'businessName'}/>
+                </div>
+                <div className="">
+                    <Label htmlFor="businessPhoto" value="business photo"/>
+                </div>
+                <ImageUploadDropZone
+                    formikProps={{
+                        setFieldError,
+                        setFieldTouched,
+                        handleBlur,
+                        handleChange,
+                        setFieldValue,
+                        fieldValue: 'businessPhoto'
+                    }}
+                    setSelectedImage={setSelectedImage}
+                />
+                <DisplayUploadErrorProps errors={errors} field={'businessPhoto'}/>
+                <div>
+                    <div>
+                        <Label htmlFor="businessHours" value="Business Hours"/>
+                    </div>
+                    <TextInput
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        autoComplete='organization'
+                        id="businessHours"
+                        name={"businessHours"}
+                        type='text'
+                        value={values.businessHours}
+                    />
+                </div>
+                <DisplayError errors={errors} touched={touched} field={'businessHours'}/>
+                <div>
+                    <div>
+                        <Label htmlFor="businessBio" value="Business Bio"/>
+                    </div>
+                    <TextInput
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        autoComplete='organization'
+                        id="businessBio"
+                        name={"businessBio"}
+                        type='text'
+                        value={values.businessBio}
+                    />
+                </div>
+                <DisplayError errors={errors} touched={touched} field={'businessBio'}/>
+                <div>
+                    <div>
+                        <Label htmlFor="businessEmail" value="Business Email"/>
+                    </div>
+                    <TextInput
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        autoComplete='email'
+                        id='businessEmail'
+                        name={"businessEmail"}
+                        type="email"
+                        value={values.businessEmail}
+                    />
+                    <DisplayError errors={errors} touched={touched} field={'businessEmail'}/>
+                </div>
+                <div>
+                    <div className='mb-2 block'>
+                        <Label htmlFor="businessPhone" value="Business Phone"/>
+                    </div>
+                    <TextInput
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        autoComplete='phone'
+                        id="businessPhone"
+                        name={'businessPhone'}
+                        type="text"
+                        value={values.businessPhone}
+                    />
+                    <DisplayError errors={errors} touched={touched} field={'businessPhone'}/>
+                </div>
+                <Button color={'success'} type="submit">Submit</Button>
+                <Button color={'failure'} type="reset" onClick={handleReset}>Reset</Button>
+                <DisplayStatus status={status}/>
+            </form>
+            <FormDebugger {...props} />
+        </>
+    )
 }
