7ddc8383877e67ed0ec02878357aa7c3cc09b08f
Rusty Libby <rlibby@cnm.edu>
2024-09-15 22:16:21 -0600

    working on section form

--- frontend/src/app/business-listing/MenuSection.tsx
+++ frontend/src/app/business-listing/MenuSection.tsx
@@ -1,8 +1,36 @@
 'use client'
 
 import {MenuItemCard} from "@/app/business-listing/MenuItemCard";
+import {SectionSchema} from "@/app/utils/models/section/section.validator";
+import {z} from "zod";
+import {Session} from "node:inspector";
+import {FormikHelpers} from "formik";
 
-export function MenuSection() {
+const menuSectionSchema = SectionSchema
+    .omit({sectionId: true, sectionBusinessId: true})
+
+type MenuSection = z.infer<typeof menuSectionSchema>
+
+type Props = {session: Session} // do we need this to show we are logged in, so we can put a section in a menu?
+
+
+export function MenuSection(props:Props) {
+    const session = props.session;
+
+
+    const initialValues  = {
+        sectionName: '',
+        sectionDescription: '',
+        sectionOrder: 0
+    }
+
+
+    const handleSubmit = (values: MenuSection, actions: FormikHelpers<MenuSection>) => {
+
+    }
+
+
+
     return(
         <>
             <h3 className='text-2xl'>Appetizers</h3>
