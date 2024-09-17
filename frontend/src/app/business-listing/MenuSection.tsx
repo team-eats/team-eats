2d26ea6c6ad8d39dca09c6bdae503d80517e38be
Rusty Libby <rlibby@cnm.edu>
2024-09-17 16:06:13 -0600

    commiting stuff

--- frontend/src/app/business-listing/MenuSection.tsx
+++ frontend/src/app/business-listing/MenuSection.tsx
@@ -9,13 +9,13 @@ type MenuSectionProps = {
 export async function MenuSection(props: MenuSectionProps) {
     const section = props.section
 
-    // const items = await fetchMenuItemByItemSectionId
+    const items = await fetchMenuItemByItemSectionId(section.sectionId)
 
     return(
         <>
             <h3 className='text-2xl'>{section.sectionName}</h3>
             <div className='overflow-x-auto flex gap-3'>
-                {/*{items.map(item =><MenuItemCard key={item.itemSectionId} item={item} />)}*/}
+                {items.map(item =><MenuItemCard key={item.itemSectionId} item={item} />)}
             </div>
         </>
     )
