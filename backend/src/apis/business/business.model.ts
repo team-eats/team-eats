6fe32cda9c49388278166f673ef43d903cb4bde0
Cooper Mcgaffey <lmcgaffey@cnm.edu>
2024-09-17 13:11:37 -0600

    trying to make search better

--- backend/src/apis/business/business.model.ts
+++ backend/src/apis/business/business.model.ts
@@ -177,9 +177,9 @@ export async function selectSearchBusinessesByName(searchTerm: string): Promise<
     FROM business
     INNER JOIN section ON section.section_business_id = business.business_id
     INNER JOIN item ON item.item_section_id = section.section_id
-    WHERE business_name LIKE ${formattedValue} 
+    WHERE business_name LIKE ${formattedValue}
     OR section_name LIKE ${formattedValue}
-    OR item_name % ${formattedValue}`
+    OR item_name LIKE ${formattedValue}`
 
     return BusinessSchema.array().parse(rowList)
 }
