6f28a9be0b3d6ab462c01ee6a431dbaffef4861a
Roel Marasigan <roelm07@yahoo.com>
2024-09-17 13:11:38 -0600

    update admin page with items

--- frontend/src/app/business-listing/[businessName]/admin/item.form.tsx
+++ frontend/src/app/business-listing/[businessName]/admin/item.form.tsx
@@ -1,7 +1,7 @@
-
+'use client'
 import {Formik, FormikHelpers, FormikProps} from "formik";
 import React from "react";
-import {Button, Label, TextInput} from "flowbite-react";
+import {Button, Label, Select, TextInput} from "flowbite-react";
 import {DisplayUploadErrorProps, ImageUploadDropZone} from "@/app/components/ImageUploadDropZone";
 import {DisplayError} from "@/app/components/DisplayError";
 import {FormDebugger} from "@/app/components/FormDebugger";
@@ -10,6 +10,7 @@ import {z} from "zod";
 import {ItemSchema} from "@/app/utils/models/items/item.validator";
 import {Session} from "@/app/utils/session.utils";
 import {toFormikValidationSchema} from "zod-formik-adapter";
+import {Section} from "@/app/utils/models/section/section.validator";
 
 const FormSchema = ItemSchema.omit({
     itemId: true,
@@ -24,10 +25,14 @@ const FormSchema = ItemSchema.omit({
 
 type ItemForm = z.infer<typeof FormSchema>
 
-type Props = {session: Session}
+type Props = {
+    session: Session,
+    sections: Section[]
+}
 
 export function CreateItemForm(props: Props) {
     const session = props.session;
+    const sections = props.sections;
 
     const initialValues = {
         itemSectionId:'',
@@ -50,23 +55,52 @@ export function CreateItemForm(props: Props) {
         }
         const {setStatus, resetForm} = actions
 
-
-        fetch("/apis/item/", {
-            method: "POST",
-            headers: {
-                'Authorization': session.authorization ?? ""
-            },
-            body: values.itemName
-        })
-            .then(response =>
-                response.json())
-            .then(json => {
-                if (json.status !== 200) {
-                    setStatus({type: 'failure', message: json.message})
-                } else {
-                    values.itemName = json.message
-                }
+        if (values.itemPhoto) {
+            fetch('/apis/image/', {
+                method: 'POST',
+                headers: {
+                    'Authorization': session.authorization ?? ""
+                },
+                body: values.itemPhoto
             })
+                .then(response => response.json())
+                .then(json => {
+                    if (json.status !== 200) {
+                        setStatus({type: 'failure', message: json.message})
+                    } else {
+                        newValues.itemPhoto = json.message
+                        postItem()
+                    }
+                })
+        } else {
+            newValues.itemPhoto = null
+            postItem()
+        }
+
+        function postItem() {
+            console.log(newValues)
+            fetch("/apis/item/", {
+                method: "POST",
+                headers: {
+                    'Authorization': session.authorization ?? ""
+                },
+                body: JSON.stringify(newValues)
+            })
+                .then(response =>
+                    response.json())
+                .then(data => {
+                    let type = 'failure'
+                    if (data.status === 200) {
+                        type = 'success'
+                        resetForm()
+                    }
+                    setStatus({type, message: data.message})
+                })
+                .catch(error => {
+                    console.log(error)
+                    setStatus({type: 'failure', message: error.message})
+                })
+        }
 
     }
 
@@ -75,7 +109,130 @@ export function CreateItemForm(props: Props) {
             <div className="bg-white p-6 rounded-lg shadow-lg w-full">
                 <Formik initialValues={initialValues} onSubmit={handleSubmit}
                         validationSchema={toFormikValidationSchema(FormSchema)}>
-                    {ItemFormContent}
+                    {(props) => {
+                        const {
+                            status,
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            handleReset,
+                            setFieldValue,
+                            setFieldError,
+                            setFieldTouched
+                        } = props;
+
+                        const [selectedImage, setSelectedImage] = React.useState<string | null> (null)
+
+                        return (
+                            <>
+                                <form onSubmit={handleSubmit}>
+                                    {selectedImage ? <img src={selectedImage} alt={"uploadedImage"}
+                                                          className="w-full h-auto"/> : <></>}
+                                    <div className="max-w-md">
+                                        <div className="mb-2 block">
+                                            <Label htmlFor="menuSection" value="Select your menu section"/>
+                                        </div>
+                                        <Select id="menuSection" required>
+                                            <option>Select an option</option>
+                                            {sections.map(section => (
+                                                <option value={section.sectionId as string}>{section.sectionName}</option>
+                                            ))}
+                                        </Select>
+                                    </div>
+
+                                    <div className="">
+                                        <Label htmlFor="itemPhoto" value="Item Photo"/>
+                                    </div>
+                                    <ImageUploadDropZone
+                                        formikProps={{
+                                            setFieldError,
+                                            setFieldTouched,
+                                            handleBlur,
+                                            handleChange,
+                                            setFieldValue,
+                                            fieldValue: 'itemPhoto'
+                                        }}
+                                        setSelectedImage={setSelectedImage}
+                                    />
+
+                                    <DisplayUploadErrorProps errors={errors} field={'itemPhoto'}/>
+
+                                    <div>
+                                        <div>
+                                            <Label htmlFor="itemName" value="Item Name"/>
+                                        </div>
+                                        <TextInput
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            autoComplete='organization'
+                                            id="itemName"
+                                            name={"itemName"}
+                                            type='text'
+                                            value={values.itemName}
+                                            className="w-full"
+                                        />
+                                    </div>
+                                    <DisplayError errors={errors} touched={touched} field={'itemName'}/>
+                                    <div>
+                                        <div>
+                                            <Label htmlFor="itemDescription" value="Item Description"/>
+                                        </div>
+                                        <TextInput
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            autoComplete='organization'
+                                            id="itemDescription"
+                                            name={"itemDescription"}
+                                            type='text'
+                                            value={values.itemDescription}
+                                            className="w-full"
+                                        />
+                                    </div>
+                                    <DisplayError errors={errors} touched={touched} field={'itemDescription'}/>
+                                    <div>
+                                        <div>
+                                            <Label htmlFor="itemPrice" value="Item Price"/>
+                                        </div>
+                                        <TextInput
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            autoComplete='organization'
+                                            id="itemPrice"
+                                            name={"itemPrice"}
+                                            type='text'
+                                            value={values.itemPrice}
+                                            className="w-full"
+                                        />
+                                    </div>
+                                    <DisplayError errors={errors} touched={touched} field={'itemPrice'}/>
+                                    <div>
+                                        <div>
+                                            <Label htmlFor="itemOrder" value="Item Order"/>
+                                        </div>
+                                        <TextInput
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            autoComplete='organization'
+                                            id="itemOrder"
+                                            name={"itemOrder"}
+                                            type='number'
+                                            value={values.itemOrder}
+                                            className="w-full"
+                                        />
+                                        <DisplayError errors={errors} touched={touched} field={'itemOrder'}/>
+                                    </div>
+                                    <Button color={'success'} type="submit">Submit</Button>
+                                    <Button color={'failure'} type="reset" onClick={handleReset}>Reset</Button>
+                                    <DisplayStatus status={status}/>
+                                </form>
+                                <FormDebugger {...props} />
+                            </>
+                        )
+
+                    }}
                 </Formik>
             </div>
         </div>
@@ -83,134 +240,5 @@ export function CreateItemForm(props: Props) {
 }
 
 
-const [selectedItem, setSelectedName] = React.useState<string | null> (null)
 
-export function ItemFormContent(props: FormikProps<ItemForm>)
-{
-    const {
-        status,
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        handleReset,
-        setFieldValue,
-        setFieldError,
-        setFieldTouched
-    } = props;
 
-    const [selectedImage, setSelectedImage] = React.useState<string | null> (null)
-
-    return (
-        <>
-            <form onSubmit = {handleSubmit}>
-                {selectedImage ? <img src={selectedImage} alt={"uploadedImage"}
-                                      className="w-full h-auto"/> : <></>}
-                <div>
-                    <div>
-                        <Label htmlFor="itemName" value="Item Name"/>
-                    </div>
-                    <TextInput
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        autoComplete='organization'
-                        id='itemName'
-                        name={'itemName'}
-                        type='text'
-                        value={values.itemName}
-                        className="w-full"
-                    />
-                    <DisplayError errors={errors} touched={touched} field={'itemName'}/>
-                </div>
-                <div className="">
-                    <Label htmlFor="itemPhoto" value="Item Photo"/>
-                </div>
-                <ImageUploadDropZone
-                    formikProps={{
-                        setFieldError,
-                        setFieldTouched,
-                        handleBlur,
-                        handleChange,
-                        setFieldValue,
-                        fieldValue: 'itemPhoto'
-                    }}
-                    setSelectedImage={setSelectedImage}
-                />
-
-                <DisplayUploadErrorProps errors={errors} field={'itemPhoto'}/>
-
-                <div>
-                    <div>
-                        <Label htmlFor="itemName" value="Item Name"/>
-                    </div>
-                    <TextInput
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        autoComplete='organization'
-                        id="itemName"
-                        name={"itemName"}
-                        type='text'
-                        value={values.itemName}
-                        className="w-full"
-                    />
-                </div>
-                <DisplayError errors={errors} touched={touched} field={'itemName'} />
-                <div>
-                    <div>
-                        <Label htmlFor="itemDescription" value="Item Description"/>
-                    </div>
-                    <TextInput
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        autoComplete='organization'
-                        id="itemDescription"
-                        name={"itemDescription"}
-                        type='text'
-                        value={values.itemDescription}
-                        className="w-full"
-                    />
-                </div>
-                <DisplayError errors={errors} touched={touched} field={'itemDescription'} />
-                <div>
-                    <div>
-                        <Label htmlFor="itemPrice" value="Item Price"/>
-                    </div>
-                    <TextInput
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        autoComplete='organization'
-                        id="itemPrice"
-                        name={"itemPrice"}
-                        type='text'
-                        value={values.itemPrice}
-                        className="w-full"
-                    />
-                </div>
-                <DisplayError errors={errors} touched={touched} field={'itemPrice'} />
-                <div>
-                    <div>
-                        <Label htmlFor="itemOrder" value="Item Order"/>
-                    </div>
-                    <TextInput
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        autoComplete='organization'
-                        id="itemOrder"
-                        name={"itemOrder"}
-                        type='text'
-                        value={values.itemOrder}
-                        className="w-full"
-                    />
-                    <DisplayError errors={errors} touched={touched} field={'itemOrder'} />
-                </div>
-                <Button color={'success'} type="submit">Submit</Button>
-                <Button color={'failure'} type="reset" onClick={handleReset}>Reset</Button>
-                <DisplayStatus status={status}/>
-            </form>
-            <FormDebugger {...props} />
-        </>
-    )
-
-}
