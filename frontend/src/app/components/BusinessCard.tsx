aef0dd5ff88592e73a9f2e775c87caac7079caf0
Cooper Mcgaffey <lmcgaffey@cnm.edu>
2024-09-18 09:14:42 -0600

    conflicsts with diplaying images

--- frontend/src/app/components/BusinessCard.tsx
+++ frontend/src/app/components/BusinessCard.tsx
@@ -25,7 +25,10 @@ export function BusinessCard(props: businessCardProps) {
     return (
         <Card
             className="min-w-[16rem] max-w-[16rem] h-[22rem] box-border mx-auto container"
-            renderImage={() => <Link href={`/business-listing/${business.businessName}`}><Image width={150} height={200} src="/${business.businessPhoto}" alt="image 1" /></Link>}>
+            renderImage={() =>
+                <Link href={`/business-listing/${business.businessName}`}>
+                    <Image width={150} height={200} src="/${business.businessPhoto}" alt="image 1" />
+                </Link>}>
 
             <ReactCardFlip flipDirection={'horizontal'} isFlipped={isFlipped}>
                 <div onClick={flipCard}>
--- frontend/src/app/components/RandomButton.tsx
+++ frontend/src/app/components/RandomButton.tsx
@@ -1,13 +1,16 @@
+
+'use client'
+
 import {fetchAllBusinesses} from "@/app/utils/models/business/business.model";
 import {Business} from "@/app/utils/models/business/business.validator";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, {useState} from "react";
 
 
-export async function RandomButton() {
+export async function RandomButton(props:{businesses: Business[]}) {
 
-    const allBusinesses = await fetchAllBusinesses();
+    const randomIndex =() => Math.floor(Math.random() * props.businesses.length);
 
     function shuffle (array: Business[]) {
         for (let i = array.length; i > 0; i--) {
@@ -16,16 +19,47 @@ export async function RandomButton() {
 
             [array[i], array[j]] = [array[j], array[i]];
 
-        } return array[array.length - 1];
+        } return array;
     }
 
-    const business = shuffle(allBusinesses);
+    const randomBusinesses = shuffle (props.businesses);
+
+    const [business, setRandomBusiness] = useState<Business>(randomBusinesses[randomIndex()]);
 
     return (
         <>
-            <button type={"button"} className={"bg-sky-800 text-white p-3 px-7 font-bold text-2xl rounded-2xl"}>
+            <button type={"button"} className={"bg-sky-800 text-white p-3 px-7 font-bold text-2xl rounded-2xl"} onClick={() => {setRandomBusiness(randomBusinesses[randomIndex()]);}}>
+
                 <Link href={`/business-listing/${business.businessName}`}>Random</Link>
+
             </button>
         </>
     )
 }
+
+
+
+// export async function RandomButton() {
+//
+//     const allBusinesses = await fetchAllBusinesses();
+//
+//     function shuffle (array: Business[]) {
+//         for (let i = array.length; i > 0; i--) {
+//
+//             let j = Math.floor(Math.random() * (i + 1));
+//
+//             [array[i], array[j]] = [array[j], array[i]];
+//
+//         } return array[array.length - 1];
+//     }
+//
+//     const business = shuffle(allBusinesses);
+//
+//     return (
+//         <>
+//             <button type={"button"} className={"bg-sky-800 text-white p-3 px-7 font-bold text-2xl rounded-2xl"}>
+//                 <Link href={`/business-listing/${business.businessName}`}>Random</Link>
+//             </button>
+//         </>
+//     )
+// }
