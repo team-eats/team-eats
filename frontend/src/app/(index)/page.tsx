8fd6a361ac1249cdcdf34afd1bd29eaef5538b41
Cooper Mcgaffey <lmcgaffey@cnm.edu>
2024-09-18 17:25:57 -0600

    working on merge conflict

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -14,9 +14,9 @@ export default async function Home() {
             <div className={"sm:container sm:mx-auto mx-4 my-8 border-2 border-black text-center p-5"}>
                 <h1 className={"text-3xl font-bold"}>Welcome to Team Eats</h1>
                 <p className={"py-5"}>Ah, now we see the violence inherent in the system! Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.</p>
-                {/*<Suspense fallback={<></>}>*/}
-                    {businesses?.length ? <RandomButton businesses={businesses} /> : ''}
-                {/*</Suspense>*/}
+
+                {businesses?.length ? <RandomButton businesses={businesses} /> : ''}
+
             </div>
 
             <div>
