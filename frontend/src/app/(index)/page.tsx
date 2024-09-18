5275acb0e30029407e4d4187d661cf5a7f0d096f
Cooper Mcgaffey <lmcgaffey@cnm.edu>
2024-09-18 17:06:21 -0600

    fixed random button crashing home

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -1,7 +1,8 @@
-
+'use server'
 import {HomepageSection} from "@/app/(index)/HomepageSection";
 import {RandomButton} from "@/app/components/RandomButton";
 import {fetchAllBusinesses} from "@/app/utils/models/business/business.model";
+import {Suspense} from "react";
 
 export default async function Home() {
 
@@ -13,15 +14,17 @@ export default async function Home() {
             <div className={"sm:container sm:mx-auto mx-4 my-8 border-2 border-black text-center p-5"}>
                 <h1 className={"text-3xl font-bold"}>Welcome to Team Eats</h1>
                 <p className={"py-5"}>Ah, now we see the violence inherent in the system! Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.</p>
-                <button>
-                    <RandomButton businesses={businesses} />
-                </button>
+                {/*<Suspense fallback={<></>}>*/}
+                    {businesses?.length ? <RandomButton businesses={businesses} /> : ''}
+                {/*</Suspense>*/}
             </div>
 
+
+
             <div>
-                <HomepageSection />
-                <HomepageSection />
-                <HomepageSection />
+                {/*<HomepageSection />*/}
+                {/*<HomepageSection />*/}
+                {/*<HomepageSection />*/}
             </div>
 
         </>
