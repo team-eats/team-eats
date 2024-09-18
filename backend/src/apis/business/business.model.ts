ec71ee59a0b9e7ecec66ed0591be434a52d354c4
Cooper Mcgaffey <lmcgaffey@cnm.edu>
2024-09-18 11:49:52 -0600

    working on search

--- backend/src/apis/business/business.model.ts
+++ backend/src/apis/business/business.model.ts
@@ -177,7 +177,7 @@ export async function selectSearchBusinessesByName(searchTerm: string): Promise<
     FROM business
     INNER JOIN section ON section.section_business_id = business.business_id
     INNER JOIN item ON item.item_section_id = section.section_id
-    WHERE business_name levenshtein(business_name, ${searchTerm}) >=2
+    WHERE business_name levenshtein(lower(business_name, ${searchTerm})) <= 3
     OR section_name LIKE (LOWER(${formattedValue}))
     OR item_name LIKE (LOWER(${formattedValue}))`
 
--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -19,23 +19,22 @@ export default async function results(props: PageProps<{}, SearchParams>){
         results = await fetchAllBusinesses();
     }
 
-        return (
-            <>
-                <section className={"container mx-auto "}>
-                    {
-                        !q ?? <>
-                            <div>
-                                <h1 className={"text-4xl text-center pt-20 my-10 text-sky-800"}>
-                                    <span>Sorry! Try these instead!</span></h1>
-                            </div>
-                        </>
-                    }
+    return (
+        <>
+            <section className={"container mx-auto "}>
+                {
+                    !q ?? <>
+                        <div>
+                            <h1 className={"text-4xl text-center pt-20 my-10 text-sky-800"}>
+                                <span>Sorry! Try these instead!</span></h1>
+                        </div>
+                    </>
+                }
 
-                    <div className="md:grid-cols-2 xl:grid-cols-4 grid grid-rows-1 gap-8 ">
-                        {results.map(business => <BusinessCard key={business.businessId} business={business}/>)}
-                    </div>
-                </section>
-            </>
-        )
-
+                <div className="md:grid-cols-2 xl:grid-cols-4 grid grid-rows-1 gap-8 ">
+                    {results.map(business => <BusinessCard key={business.businessId} business={business}/>)}
+                </div>
+            </section>
+        </>
+    )
 }
